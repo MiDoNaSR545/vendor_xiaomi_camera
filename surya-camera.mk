9c6e26f93518bb986a36e3896d5770c90458ebd0
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2026-02-28 03:46:51 +0200

    Update surya-camera.mk

--- surya-camera.mk
+++ surya-camera.mk
@@ -44,10 +44,10 @@ PRODUCT_COPY_FILES += \
 
 # CameraX Config Overwrite
 PRODUCT_COPY_FILES += \
-     $(CAMERA_PATH)/configs/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt
+     $(MIUICAMERA_PATH)/configs/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt
 
 PRODUCT_COPY_FILES += \
-     $(CAMERA_PATH)/configs/camera/device_features/product/etc/device_features/sweet.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/alioth.xml
+     $(MIUICAMERA_PATH)/configs/camera/device_features/product/etc/device_features/sweet.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/alioth.xml
 
 # MIUI Camera Overlay to Surya and Karna
 PRODUCT_PACKAGES += \
